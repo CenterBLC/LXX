48a33c1f1a552609bf58aa1cbb9a4301de8c034d
Oliver Glanz <o.glanz@gmail.com>
2021-12-28 02:40:48 -0500

    Add files via upload
    Initial release of the Rahlfs 1935 LXX edition.

---
+++ otype.tf
@@ -0,0 +1,17 @@
+@node
+@Author=Rahlfs
+@Converter=Adrian Negrea, Oliver Glanz
+@Editors=CCAT, Eliran Wong
+@Name=LXX
+@Note=?
+@Source:=https://github.com/eliranwong/LXX-Rahlfs-1935
+@Version=1935
+@valueType=str
+@writtenBy=Text-Fabric
+@dateWritten=2021-12-24T02:16:16Z
+
+1-623693	word
+623694-623750	book
+623751-624942	chapter
+624943-655361	subverse
+655362-685732	verse
